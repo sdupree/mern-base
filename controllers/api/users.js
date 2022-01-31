aaa0072178195e77f902fbba1275b2a9dcb6ed31
Shane Dupree <shane.dupree@gmail.com>
2022-01-31 16:47:51 -0500

    Add user model.

---
+++ controllers/api/users.js
@@ -0,0 +1,10 @@
+module.exports = { create };
+
+async function create(req, res) {
+  res.json({
+    user: {
+      name: req.body.name,
+      email: req.body.email
+    }
+  });
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const usersCtrl = require('../../controllers/api/users.js');
+
+// POST /api/users
+router.post('/', usersCtrl.create);
+
+module.exports = router;
